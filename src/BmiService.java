66ac53d5ceef7f8b9b9835623205e183e1e17a26
Neto test <test@neto.com>
2022-12-24 10:33:29 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double calculate(double height,double weight){
-        double heightm = height/100;
+    public double calculate(double height, double weight) {
+        double heightm = height / 100;
         double height2 = heightm * heightm;
-        double cost = weight/height2;
+        double cost = weight / height2;
         return cost;
 
     }
